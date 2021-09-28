10fbdcab93d09f6859f2f0a9c99301081103ac6d
Giosuè Sulipano <sulipano.gs@gmail.com>
2021-09-28 12:21:04 +0200

    Merge daily_scraper and scraper

--- utils/scraper.py
+++ utils/scraper.py
@@ -11,61 +11,51 @@ def retrieve_menu(course):
     if course == 'pizza':
         return scraper(_PIZZA)
     elif course == 'daily':
-        return daily_scraper()
+        return scraper(_DAILY)
     elif course == 'salad':
         return scraper(_SALAD)
 
 def scraper(url):
     """
-    This function scrapes pizzas and salads data from the official website.
+    This function scrapes courses data from the official website.
     """
+    
     req = requests.get(url)
     soup = BeautifulSoup(req.content, 'html.parser')
-    
+
     # List with all courses requested
     courses = []
-    
-    # Get all courses names
-    for course_name in soup.find_all('h3', 'p1'):
-        courses.append({'name': course_name.text, 'desc': '', 'price': ''})
-    
-    # Get all courses descriptions
-    i = 0
-    for course_desc in soup.find_all('p', 'p2'):
-        courses[i]['desc'] = course_desc.text
-        i += 1
-    
-    # Get all courses prices
-    # !! Now it successfully retrieves all prices but keeps putting someone out of range causing a crash
-    k = 0
-    for course_price in soup.find_all(string=re.compile('euro')):
-        courses[k]['price'] = course_price.text
-        k += 1 
+
+    if url == _DAILY:
+        # Get courses tables
+        tables = soup.find_all('table')
+
+        # Get first course names and prices
+        for row in tables[0].tbody.find_all('tr'):
+            table_cell = row.find_all('td')
+            courses.append({'name': table_cell[0].text, 'price': table_cell[1].text})
+        
+        # Get main course names and prices
+        for row in tables[1].tbody.find_all('tr'):
+            table_cell = row.find_all('td')
+            courses.append({'name': table_cell[0].text, 'price': table_cell[1].text})
+    else:        
+        # Get all courses names
+        for course_name in soup.find_all('h3', 'p1'):
+            courses.append({'name': course_name.text, 'desc': '', 'price': ''})
+        
+        # Get all courses descriptions
+        i = 0
+        for course_desc in soup.find_all('p', 'p2'):
+            courses[i]['desc'] = course_desc.text
+            i += 1
+        
+        # Get all courses prices
+        # !! Now it successfully retrieves all prices but keeps putting someone out of range causing a crash
+        k = 0
+        for course_price in soup.find_all(string=re.compile('euro')):
+            courses[k]['price'] = course_price.text
+            k += 1 
 
     # Return courses requested
     return courses
-
-def daily_scraper():
-    req = requests.get(_DAILY)
-    soup = BeautifulSoup(req.content, 'html.parser')
-
-    # List with all courses requested
-    courses = []
-
-    # Get courses tables
-    tables = soup.find_all('table')
-
-    # Get first course names and prices
-    for row in tables[0].tbody.find_all('tr'):
-        table_cell = row.find_all('td')
-        courses.append({'name': table_cell[0].text, 'price': table_cell[1].text})
-    
-    # Get main course names and prices
-    for row in tables[1].tbody.find_all('tr'):
-        table_cell = row.find_all('td')
-        courses.append({'name': table_cell[0].text, 'price': table_cell[1].text})
-
-    return courses
-
-if __name__ == '__main__':
-    print(daily_scraper())
